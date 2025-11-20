a1c928915ff294971326977e5b438dca29c03e06
apple <apple@MacBook-Air-apple.local>
2025-11-20 10:54:11 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,125 @@
+// "use strict";
+
+// let number = 4;
+
+// console.log(-4/0);
+// console.log('sting' * 2);
+
+// const person = 'alex';
+// const bool = false;
+
+// console.log(two);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: "30",
+//     isMarried: false
+// };
+
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpeg', 6,  'apple.bmp'];
+// console.log(arr[1]);
+
+// const arr = ['a', 'b', 'c'];
+
+// arr[10] = '3333';
+
+// console.log(arr[10]);
+
+// const arrObj = {
+//     0: 'a',
+//     1: 'b',
+//     2: 'c',
+//     abc: {
+//         df: [{}, {}],
+//         def: {
+
+//         }
+//     }
+// };
+// const b = 'b';
+
+// arrObj[b] = '1234';
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// const obj = {
+//     Anna: 500,
+//     'Alice' : 800
+// };
+
+// let storeName = 'Apple'
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Alex', 'Anna', 'Sam'],
+//     products: {
+//         phone: 200,
+//         mouse: 50
+//     },
+//     open: true
+// };
+// console.log(storeDescription);
+
+// "use strict";
+
+
+// const result = confirm('How are you?');
+// console.log('result');
+
+// const answer = +prompt("How old are you? 18+ ?", "18");
+// console.log(answer + 5);
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше Имя ?', '');
+// answers[1] = prompt('Как ваша фамилия ?', '');
+// answers[2] = prompt('Сколько вам лет ?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+
+// Интерполяция
+// "use strict";
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`Hello, ${user}`);
+
+
+
+// Операторы JS
+"use strict";
+
+// console.log(4 + " - object");
+
+// // Инкремент и декремент
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+// console.log(++incr);   
+// console.log(--decr); 
+
+// console.log(5%2);
+
+// console.log(2*4 == 8); //true
+// & // Оператор И
+// || // Оператор ИЛИ
+
+const isChecked = false,
+      isClose = false;
+console.log(isChecked || !isClose);
+
+console.log(2 + 2 * 2 === 8);
